2bf59844578ede156ccc967078cff62278d5f49f
Josh Larsen <larsen.bass@gmail.com>
2019-08-19 14:48:31 -0600

    added local storage for stringList

--- lib/views/myshards.dart
+++ lib/views/myshards.dart
@@ -1,7 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-class MyShards extends StatelessWidget {
 
+class MyShards extends StatefulWidget {
+  @override
+  _MyShardsState createState() => _MyShardsState();
+}
+
+class _MyShardsState extends State<MyShards> {
+  var myShards = ['nothing'];
+
+  @override
+  void initState() {
+    super.initState();
+    getShards();
+  }
+
+  saveShards(shard) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (prefs.getStringList('shards') != null) {
+      var shards = prefs.getStringList('shards');
+      shards.add(shard);
+      prefs.setStringList('shards', shards);
+    } else {
+      var shards = <String>[shard];
+      prefs.setStringList('shards', shards);
+    }
+    getShards();
+  }
+
+  getShards() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (prefs.getStringList('shards') != null) {
+    setState(() => myShards = prefs.getStringList('shards'));
+    } else {
+      setState(() => myShards = ['nothing']);
+    }
+  }
+
+  destroyShards() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('shards');
+    getShards();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +52,17 @@ class MyShards extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text("Hello"),
+            Text(myShards.toString()),
+            FlatButton(
+              onPressed: () {saveShards('chicken');
+              },
+              child: Text('Add Shard')
+            ),
+            FlatButton(
+              onPressed: () {destroyShards();
+              },
+              child: Text('Erase All Shards')
+            ),
          ]
         )
       )
