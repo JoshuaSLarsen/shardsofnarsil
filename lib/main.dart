dda4b5ec49c5617b58afd3b2b02fa6576e4c5046
Josh Larsen <larsen.bass@gmail.com>
2019-08-20 19:44:32 -0600

    force portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './navigation/home.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,6 +8,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Shards of Narsil',
       theme: ThemeData(
